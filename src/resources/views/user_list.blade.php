590aff5d3ed0ccffa485a4ade6540901990d270d
toshiyuki <wakita0520@gmail.com>
2026-03-16 21:43:42 +0900

    3/16 CSSファイル作成

---
+++ src/resources/views/user_list.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.user_app') 
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/index.css') }}" />
+@endsection 
+
+@section('content')
+<div class="layout">
+    
+</div>
+
+@endsection
+
